4b4c31597d1560cd652ceee5cc2b3deed72d3b9e
Marcus Elliott <webdevhero2010@gmail.com>
2021-10-23 14:09:53 -0400

    updated index.js in controller folder

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,8 @@
 module.exports = {
   HealthDeptUser: require("./healthdeptuserController"),
   Facility: require("./facilitycontroller"),
+  FacilityOwner: require("./facilityownercontroller"),
+  FoodHandler: require("./foodhandlercontroller"),
+  InspectionReport: require("./inspectionreportscontroller"),
+  Narratives: require("./narrativescontroller"),
 };
